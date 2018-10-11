2fc8295923b36ad5d35c40f6347e40e98379a4de
Md. Rokonuzzaman <rzrokon@gmail.com>
2018-10-11 17:26:36 +0600

    migrate to uuid with single migration and make uuid field primary then uuid rename to id

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from . models import Books
+from . models import Books, Movie
 
 admin.site.register(Books)
+admin.site.register(Movie)
--- webapp/models.py
+++ webapp/models.py
@@ -11,3 +11,16 @@ class Books(models.Model):
 
     def __str__(self):
         return '%s by %s' % (self.name, self.author)
+
+
+class Movie(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True)
+    name = models.CharField(max_length=100)
+    genres = models.CharField(max_length=100)
+    year = models.CharField(max_length=50)
+    rating = models.CharField(max_length=4)
+    release = models.DateField(null=False)
+
+    def __str__(self):
+        return '%s - %s' % (self.name, self.year)
+
